666cf0bee839612adb57ff9db5319f316ab9b5ee
Mauricio <mauricio.179728@gmail.com>
2023-10-22 19:16:53 -0500

    inicio de graficos

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+//Importar componentes o librerias
+
+import "bootstrap/dist/css/bootstrap.min.css";
+
+
+
+
+//Importar elementos CSS (estilos)
+import "./App.css"; //importar stilos css de app.css
+
+import Client from "./components/Client";
+
+
+//Crear el compnente
+function App() {
+  //renderizar el componente
+  return (
+    <div>
+
+     <Client/>
+     
+    
+
+
+
+
+
+
+    </div>
+  );
+}
+
+//exportar componente
+export default App;
